cad519e3961c92458e23b9e127844e5f9337ba96
Tom Hacquoil <thacquoil@infuse-ipm.com>
2010-10-11 15:30:47 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '7001853bf070121ccf011823e18f80b53fd35b765d92396caa433f99e9b3672443813d86229dc1fe8de7328748acd28fb25753f108c534f83151211787283fa6'
